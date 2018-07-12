1c71cab62513ac69c97ecfef772f69a70ad0098b
wendf <wdf163mail@163.com>
2018-07-12 18:19:28 +0800

    使用Sequelize框架

---
+++ app/models/world_cup.js
@@ -0,0 +1,80 @@
+var Sequelize = require('sequelize');
+var sequelize = require('../../config/sequelize_db');//数据库配置
+
+/* jshint indent: 1 */
+
+exports.db = () => {
+	return sequelize.define('fundoutOrder', {
+		id: {
+			type: Sequelize.BIGINT(16).UNSIGNED,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+			field: 'id'
+		},
+		match_group: {
+            type: Sequelize.STRING(25),
+            allowNull: true,
+			field: 'match_group'
+		},
+		match_name: {
+            type: Sequelize.STRING(100),
+            allowNull: true,
+			field: 'match_name'
+		},
+		video_url: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+			field: 'video_url'
+		},
+		match_date: {
+			type: Sequelize.DATE,
+			allowNull: true,
+			field: 'match_date'
+		},
+		home_team: {
+            type: Sequelize.STRING(20),
+            allowNull: true,
+			field: 'home_team'
+        },
+        home_badge: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+			field: 'home_badge'
+		},
+		home_goal: {
+            type: Sequelize.STRING(5),
+            allowNull: true,
+			field: 'home_goal'
+        },
+		away_team: {
+			type: Sequelize.STRING(20),
+			allowNull: true,
+			field: 'away_team'
+        },
+        away_badge: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+			field: 'away_badge'
+		},
+		away_goal: {
+            type: Sequelize.STRING(5),
+            allowNull: true,
+			field: 'away_goal'
+        },
+        victory: {
+			type: Sequelize.STRING(20),
+			allowNull: true,
+			field: 'victory'
+        },
+        create_time: {
+			type: Sequelize.DATE,
+			allowNull: true,
+			field: 'create_time'
+		}
+	}, {
+		tableName: 'world_cup',
+		timestamps: false,
+		underscoredAll: false//驼峰规则
+	});
+};
--- app/service/worldcupService.js
+++ app/service/worldcupService.js
@@ -1,33 +1,40 @@
 const mysql = require('mysql');
-const config =  require(`../../config/mysql_test.js`);
+const config =  require(`../../config/mysql_test`);
+const worldCup = require('../models/world_cup')
 
-exports.getdata = async data => {//data请求参数
-    //创建mysql连接
-    var connection = mysql.createConnection({
-        host : config.host,
-        user : config.user,
-        password : config.password,
-        database : config.database
-    });
-    connection.connect();
+// exports.getdata = async data => {//data请求参数
+//     //创建mysql连接
+//     var connection = mysql.createConnection({
+//         host : config.host,
+//         user : config.user,
+//         password : config.password,
+//         database : config.database
+//     });
+//     connection.connect();
 
-    let datas = new Array();
-    await new Promise(async (carryon1)=> {
-        //记录到数据库
-        connection.query(`SELECT id,match_group,match_name,video_url,match_date,home_team,home_badge,home_goal,away_team,away_badge,away_goal,victory,create_time FROM world_cup WHERE 1=1`, (error, results, fields)=> {
-            if (error) {
-                console.log(`数据库查询出错:${error}`);
-                // throw error;
-            };
-            if(results.length > 0){
-                for(let i in results){
-                    datas.push(results[i]);
-                }
-                carryon1();
-            }
-        });
-    });
+//     let datas = new Array();
+//     await new Promise(async (carryon1)=> {
+//         //记录到数据库
+//         connection.query(`SELECT id,match_group,match_name,video_url,match_date,home_team,home_badge,home_goal,away_team,away_badge,away_goal,victory,create_time FROM world_cup WHERE 1=1`, (error, results, fields)=> {
+//             if (error) {
+//                 console.log(`数据库查询出错:${error}`);
+//                 // throw error;
+//             };
+//             if(results.length > 0){
+//                 for(let i in results){
+//                     datas.push(results[i]);
+//                 }
+//                 carryon1();
+//             }
+//         });
+//     });
 
-    connection.end();
+//     connection.end();
+//     return datas;
+// }
+
+//使用sequelize框架
+exports.getdata = async data=>{
+    let datas = await worldCup.db().findAll();
     return datas;
-}
+};
